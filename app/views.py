8487140e9806883190a31453c2fccaf4357d8317
Edwike12 <nyaunchoedwike@gmail.com>
2022-01-13 23:02:18 +0300

    save changes

--- app/views.py
+++ app/views.py
@@ -2,11 +2,10 @@ from flask import render_template
 from app import app
 
 # Views
-@app.route('/movie/<int:movie_id>')
-def index(movie_id):
+@app.route('/pitch/<int:pitch_id>')
+def index(pitch_id):
 
     '''
     View root page function that returns the index page and its data
     '''
-    message = 'Hello World'
-    return render_template('index.html',message = message)
+    return render_template('one_minute_pitch.html',id = pitch_id)
